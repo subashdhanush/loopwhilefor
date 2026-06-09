d47ee44fe70963434813f1f3de6ea298abe546ca
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2026-06-09 10:16:17 +0530

    Add files via upload

---
+++ f1.py
@@ -0,0 +1,7 @@
+# For loop: repeat 10 times
+for i in range(10):
+    print("Hello")
+
+
+# Use a for loop when you know how many times the loop should run.
+# Use a while loop when the loop should continue until a specific condition changes.
